4a5ec6b46d7c6eac6c5aadb953b0017aafda5aba
matt <86740915+Matt-develop@users.noreply.github.com>
2023-11-05 16:45:26 -0300

    correção do js do slider

--- slider/script.js
+++ slider/script.js
@@ -1,3 +1,4 @@
+const bannerBtn = document.querySelectorAll(".manual-btn");
 var radio = document.querySelector('.manual-btn');
 var cont = 1;
 
@@ -6,6 +7,14 @@ document.getElementById('radio1').checked = true;
 setInterval(function() {
     proximaImg();
 }, 5000);
+
+bannerBtn.forEach(button => {
+
+    button.addEventListener("click", () => {
+        cont = button.id;
+    });
+
+});
 
 function proximaImg() {
 
