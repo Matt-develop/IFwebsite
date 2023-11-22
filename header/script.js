55c0b165c61b981830a038ab98e759f7d7ae720a
Matt <mattneedstudy@gmail.com>
2023-11-22 11:28:22 -0300

    identação da pagina da galeria + cor do header

--- header/script.js
+++ header/script.js
@@ -3,8 +3,6 @@ var root = document.querySelector(":root");
 if(window.location.pathname == "/index.html") {
     root.style.setProperty('--primary', '#1073c0')
 }
-else if(window.location.pathname == "/galeriaa/galeria.html") {
+if(window.location.pathname == "/galeriaa/galeria.html") {
     root.style.setProperty('--primary', '#a9a1f9')
 }
-
-console.log(window.location.pathname)
