4aed673849103f07efb15bafed48f226676fc940
Matt <mattneedstudy@gmail.com>
2023-11-22 11:34:42 -0300

    add: cor do header em cada pag

--- header/script.js
+++ header/script.js
@@ -1,8 +1,14 @@
 var root = document.querySelector(":root");
 
-if(window.location.pathname == "/index.html") {
-    root.style.setProperty('--primary', '#1073c0')
+// home do site - verde escuro
+if(window.location.pathname == "/index.html" || window.location.pathname == "/") {
+    root.style.setProperty('--primary', '#008060')
 }
+// galeria - roxo
 if(window.location.pathname == "/galeriaa/galeria.html") {
     root.style.setProperty('--primary', '#a9a1f9')
+}
+// por que fazer - azul
+if(window.location.pathname == "/paginas/pro.html") {
+    root.style.setProperty('--primary', '#1073c0')
 }
