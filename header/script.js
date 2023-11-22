fd8c86311b754dd87b936b48c216bf1fe2566c3a
LouisechanUwU <celsohenrique.nl@gmail.com>
2023-11-22 08:43:22 -0300

    mudanças no header dagaleria- mudamo brr navegaçao

--- header/script.js
+++ header/script.js
@@ -1,5 +1,10 @@
 var root = document.querySelector(":root");
 
-if(window.location.pathname != "/index.html") {
+if(window.location.pathname == "/index.html") {
     root.style.setProperty('--primary', '#1073c0')
 }
+else if(window.location.pathname == "/galeriaa/galeria.html") {
+    root.style.setProperty('--primary', '#a9a1f9')
+}
+
+console.log(window.location.pathname)
